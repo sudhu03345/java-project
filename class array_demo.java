464bc20b5909a4de345c7c211732e773883c17dc
sudarshan v poojary <vvarunspt@gmail.com>
2025-04-08 15:20:37 +0530

    Add files via upload

---
+++ class array_demo.java
@@ -0,0 +1,19 @@
+class array_demo
+{
+	public static void main (String args[])
+	{
+		int[]=new in length();
+		int legnth=args.length():
+		int i;
+		for(i=0;1<len;i++)
+		{
+			x[i]=integer.parseInt(args[i]);
+		}
+		for(i=10;i<len;i++)
+		{
+			system.out.println(x[i]);
+		}
+		sum=sum+i
+		avg=sum/i
+	}(exception e){}
+}
---
+++ vector_demo.java
@@ -0,0 +1,21 @@
+import java.Util*;
+class vector_demo
+{
+	public static void main(String args[])
+	{
+		vector list=args.length;
+		for int(i=0,i<len;i++)
+		{
+			list add.length(args[i])
+		}
+		list insert element AT("os",1);
+		int s s=list.size();
+		String array=new String[s];
+		list.copyInt 0 (array);
+		System.out.println("list of elements:");
+		for(int i=0;i<len;i++)
+		{
+			System.out.println(list of element[i]);
+		}
+	}
+}
